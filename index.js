3d357bf2b17e0a7c761f1d44ae8a110ddaadd205
mattbechtel1 <bechma02@gmail.com>
2020-03-02 16:51:53 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+function findMinAndRemoveSorted(array) {
+    return array.shift()
+}
 
+function merge(array1, array2) {
+    let sorted = []
+
+    while (array1.length != 0 && array2.length != 0) {
+        let currentMin = array1[0] < array2[0] ? array1 : array2
+        sorted.push(findMinAndRemoveSorted(currentMin))
+    }
+    return sorted.concat(array1).concat(array2)
+}
+
+function mergeSort(unsortedArray) {
+    let midpoint = unsortedArray.length / 2
+    let firstHalf = unsortedArray.slice(0, midpoint)
+    let secondHalf = unsortedArray.slice(midpoint, unsortedArray.length)
+
+    if (unsortedArray.length < 2) {
+        return unsortedArray
+    } else {
+        return merge(mergeSort(firstHalf), mergeSort(secondHalf))
+    }
+}
